9596e2689ae75e2a46fc035c6d74cba6e995afde
Vinícius Mombach Schneider <vinicius.schneider@cwi.com.br>
2023-09-13 23:04:17 -0300

    Finalizando autenticação

--- src/main/java/med/voll/api/infra/security/SecurityConfiguration.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package med.voll.api.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,15 +11,24 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 //Desabilitar o processo padrão de autenticação do Spring security
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+    @Autowired
+    private SecurityFilter securityFilter;
 
     @Bean //Notação para expor o retorno do método(devolver para o spring)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable().sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().build();
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().authorizeHttpRequests()
+                .antMatchers(HttpMethod.POST,"/login").permitAll() //liberando login da autenticacao
+                .anyRequest().authenticated() //todas as outras requisições requerem autenticacao
+                .and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)//Filtro implementado antes do filtro padrão do spring
+                .build();
     }
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
